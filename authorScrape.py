36c6e1aa223df65be0baedb81a5cd6c5229bf326
1manker <lmanker@uwyo.edu>
2019-05-25 16:57:45 -0600

    selenium and r scripts

--- authorScrape.py
+++ authorScrape.py
@@ -1,32 +1,66 @@
-#Lucas Manker
-#import libraries
 import sys
+import time
+import subprocess as sub
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 
-url = str(sys.argv[1])
-req = requests.request("GET", url)
 
-s = req.content
+output_file_list = []
 
-soup = BeautifulSoup(s, 'html.parser')
 
-def printer(arr1, arr2):
-    file = open("C:\\Users\\Luke\\Desktop\\output.txt", "w+")
-    for x in range(len(arr1)):
-        file.write(arr1[x] + "," + arr2[x] + "\n",)
+def printer(arr1, arr2, auth_name):
+    author_string = "C:\\Users\\Luke\\Desktop\\" + auth_name + ".txt"
+    file = open(author_string, "w+")
+    output_file_list.append(author_string)
+    for i in range(len(arr1)):
+        if arr1[i] and arr2[i] and auth_name:
+            file.write(auth_name + "," + arr1[i] + "," + arr2[i] + "\n", )
+
 
 def scrape():
-    citRow = []
-    authorRow = []
-    yearRow = []
-    count = 0
+    cit_row = []
+    author_row = []
+    year_row = []
+    name = ''
+    for row in soup.findAll('div', attrs={"id": "gsc_prf_in"}):
+        name = row.text
     for row in soup.findAll('a', attrs={"class": "gsc_a_ac gs_ibl"}):
-        citRow.append(row.text)
+        cit_row.append(row.text)
     for row in soup.findAll('a', attrs={"class": "gsc_a_at"}):
-        authorRow.append(row.text)
+        author_row.append(row.text)
     for row in soup.findAll('span', attrs={"class": "gsc_a_h gsc_a_hc gs_ibl"}):
-        yearRow.append(row.text)
-    printer(citRow, yearRow)
+        year_row.append(row.text)
+    printer(cit_row, year_row, name)
 
-scrape();
+
+for x in range(len(sys.argv) - 1):
+    url = str(sys.argv[x + 1])
+    driver = webdriver.Chrome(executable_path="C://Program Files (x86)//Google//Chrome//chromedriver.exe")
+    driver.implicitly_wait(30)
+    driver.get(url)
+
+    python_button = driver.find_element_by_id("gsc_bpf_more")
+    while python_button:
+        python_button = driver.find_element_by_id("gsc_bpf_more")
+        if not python_button.is_enabled():
+            break
+        python_button.click()
+        time.sleep(1)
+    time.sleep(1)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    driver.close()
+    scrape()
+
+for k in range(len(output_file_list)):
+    input_file = open(output_file_list[k])
+    comb_file = open("C:\\Users\\Luke\\Desktop\\combo1.csv", "a")
+    if k == 0:
+        comb_file.write("Author,Citation_Count,Year" + "\n")
+    comb_file.write(input_file.read())
+    input_file.close()
+    comb_file.close()
+
+sub.Popen([r"C://Program Files//R//R-3.6.0//bin//x64//Rscript",
+           "C://Users//Luke//Desktop//summer19 Research//graphScript.r"], shell=True)
